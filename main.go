b189edd8220cd523433f277136d4d05c1c6b5ff3
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2025-03-28 15:32:28 +0500

    Refactor getBasicAuth method to use fmt.Sprintf for improved readability

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 type authenticationInfo struct {
 	username string
 	password string
 }
 
-// create the method below
 func (auth authenticationInfo) getBasicAuth() string {
-	return "Authorization: Basic " + auth.username + ":" + auth.password
+	return fmt.Sprintf("Authorization: Basic %s:%s",auth.username,auth.password)
+	// return "Authorization: Basic " + auth.username + ":" + auth.password
 }
