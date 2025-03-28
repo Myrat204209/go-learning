8400e45349fa2497b5a3da9f7a230a59b6e78bda
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2025-03-28 16:39:43 +0500

    Add SendMessage method to User and update tests for message handling

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
+func (u User) SendMessage(message string, messageLength int) (string, bool) {
+	if messageLength > u.MessageCharLimit {
+		return "", false
+	}
+	return message, true
+}
+
+// don't touch below this line
+
 type User struct {
 	Name string
 	Membership
 }
 
 type Membership struct {
-	Type string
+	Type             string
 	MessageCharLimit int
 }
+
 func newUser(name string, membershipType string) User {
-	msgChartLimit := 100
+	membership := Membership{Type: membershipType}
 	if membershipType == "premium" {
-		msgChartLimit = 1000
+		membership.MessageCharLimit = 1000
+	} else {
+		membership.Type = "standard"
+		membership.MessageCharLimit = 100
 	}
-	return User{
-		Name: name,
-		Membership: Membership{
-			Type: membershipType,
-			MessageCharLimit: msgChartLimit,
-		},
-	}
+	return User{Name: name, Membership: membership}
 }
