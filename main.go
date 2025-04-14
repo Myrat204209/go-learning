bb8e1748c0003619b61063ea237dcc15736285bd
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2025-04-14 18:00:10 +0500

    Refactor expense handling to use float64 for cost calculations and update tests accordingly

--- main.go
+++ main.go
@@ -1,30 +1,49 @@
 package main
 
-import "fmt"
+func getExpenseReport(e expense) (string, float64) {
+	tp, ok := e.(email)
+	if !ok {
+		return "", 0.0
+	} else {
+		return tp.toAddress, tp.cost()
+	}
+
+}
+
+// don't touch below this line
 
 type expense interface {
-	cost() int
-}
-
-type formatter interface {
-	format() string
-}
-
-func (e email) cost() int {
-	if !e.isSubscribed {
-		return 5 * len(e.body)
-	}
-	return 2 * len(e.body)
-}
-
-func (e email) format() string {
-	if e.isSubscribed {
-		return fmt.Sprintf("'%s' | Subscribed", e.body)
-	}
-	return fmt.Sprintf("'%s' | Not Subscribed", e.body)
+	cost() float64
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
 }
