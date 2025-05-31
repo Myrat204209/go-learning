b880bfb8121fddc776a403dffbec236e40f464f8
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-31 21:14:56 +0500

    CH8 - L1

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func countConnections(groupSize int) int {
-	return (groupSize - 1) * groupSize / 2
+func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
+	str := [3]string{primary, secondary, tertiary}
+	var cost [3]int
+	cost[0] = len(primary)
+	for i := 1; i < 3; i++ {
+		cost[i] = cost[i-1] + len(str[i])
+	}
+	return str, cost
+
 }
