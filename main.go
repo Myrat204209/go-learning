b7e2420bf0b1cf8fd461f0217428cbd583e12b5b
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-31 23:17:48 +0500

    CH8 : Slices - L18 :  Password Strength

--- main.go
+++ main.go
@@ -1,46 +1,21 @@
 package main
 
-type Message interface {
-	Type() string
-}
-
-type TextMessage struct {
-	Sender  string
-	Content string
-}
-
-func (tm TextMessage) Type() string {
-	return "text"
-}
-
-type MediaMessage struct {
-	Sender    string
-	MediaType string
-	Content   string
-}
-
-func (mm MediaMessage) Type() string {
-	return "media"
-}
-
-type LinkMessage struct {
-	Sender  string
-	URL     string
-	Content string
-}
-
-func (lm LinkMessage) Type() string {
-	return "link"
-}
-
-// Don't touch above this line
-
-func filterMessages(messages []Message, filterType string) []Message {
-	var filtered []Message
-	for _, msg := range messages {
-		if msg.Type() == filterType {
-			filtered = append(filtered, msg)
+func isValidPassword(password string) bool {
+	if len(password) < 5 || len(password) > 12 {
+		return false
+	}
+	hasUpper, hasDigit := false, false
+	for _, char := range password {
+		if char >= 'A' && char <= 'Z' {
+			hasUpper = true
+		}
+		if char >= '0' && char <= '9' {
+			hasDigit = true
 		}
 	}
-	return filtered
+	if hasUpper && hasDigit {
+		return true
+	} else {
+		return false
+	}
 }
