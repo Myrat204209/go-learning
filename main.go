02ddf1bed74c8651a987ee59026eea9d48fe55a8
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-31 19:40:32 +0500

    CH7 - L6

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func printPrimes(max int) {
-	fmt.Println("2")
-	for i := 3; i <= max; i += 2 {
-		fl := false
-		for j := 3; j*j <= i; j += 2 {
-			if i%j == 0 {
-				fl = true
-				continue
-			}
-		}
-		if fl == false {
-			fmt.Println(i)
-		}
-	}
-}
-
-// don't edit below this line
-
-func test(max int) {
-	fmt.Printf("Primes up to %v:\n", max)
-	printPrimes(max)
-	fmt.Println("===============================================================")
-}
-
-func main() {
-	test(10)
-	test(20)
-	test(30)
+func countConnections(groupSize int) int {
+	return (groupSize - 1) * groupSize / 2
 }
--- main_test.go
+++ main_test.go
@@ -1,67 +1,69 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"testing"
-// )
+import (
+	"fmt"
+	"testing"
+)
 
-// func Test(t *testing.T) {
-// 	type testCase struct {
-// 		costMultiplier   float64
-// 		maxCostInPennies int
-// 		expected         int
-// 	}
+func TestCountGroupConnections(t *testing.T) {
+	type testCase struct {
+		groupSize int
+		expected  int
+	}
 
-// 	runCases := []testCase{
-// 		{1.1, 5, 4},
-// 		{1.3, 10, 5},
-// 		{1.35, 25, 7},
-// 	}
+	runCases := []testCase{
+		{1, 0},
+		{2, 1},
+		{3, 3},
+		{4, 6},
+	}
 
-// 	submitCases := append(runCases, []testCase{
-// 		{1.2, 1, 1},
-// 		{1.2, 15, 7},
-// 		{1.3, 20, 7},
-// 	}...)
+	submitCases := append(runCases, []testCase{
+		{0, 0},
+		{10, 45},
+		{100, 4950},
+		{1000, 499500},
+	}...)
 
-// 	testCases := runCases
-// 	if withSubmit {
-// 		testCases = submitCases
-// 	}
-// 	skipped := len(submitCases) - len(testCases)
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
 
-// 	passCount := 0
-// 	failCount := 0
+	skipped := len(submitCases) - len(testCases)
 
-// 	for _, test := range testCases {
-// 		output := getMaxMessagesToSend(test.costMultiplier, test.maxCostInPennies)
-// 		if output != test.expected {
-// 			failCount++
-// 			t.Errorf(`---------------------------------
-// Inputs:     (%v, %v)
-// Expecting:  %v
-// Actual:     %v
-// Fail
-// `, test.costMultiplier, test.maxCostInPennies, test.expected, output)
-// 		} else {
-// 			passCount++
-// 			fmt.Printf(`---------------------------------
-// Inputs:     (%v, %v)
-// Expecting:  %v
-// Actual:     %v
-// Pass
-// `, test.costMultiplier, test.maxCostInPennies, test.expected, output)
-// 		}
-// 	}
+	passCount := 0
+	failCount := 0
 
-// 	fmt.Println("---------------------------------")
-// 	if skipped > 0 {
-// 		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
-// 	} else {
-// 		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
-// 	}
-// }
+	for _, test := range testCases {
+		result := countConnections(test.groupSize)
+		if result != test.expected {
+			failCount++
+			t.Errorf(`---------------------------------
+Group Size: %v
+Expecting: %v
+Actual:    %v
+Fail
+`, test.groupSize, test.expected, result)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Group Size: %v
+Expecting: %v
+Actual:    %v
+Pass
+`, test.groupSize, test.expected, result)
+		}
+	}
 
-// // withSubmit is set at compile time depending
-// // on which button is used to run the tests
-// var withSubmit = true
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
+var withSubmit = true
