d6f5f64969eec0aee16aa4e321c3db3f1c4ccca6
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-06-05 19:12:18 +0500

    CH9: Maps - L5 :  Count Instances

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
-import "errors"
-
-func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
-	elem, ok := users[name]
-	if !ok {
-		return false, errors.New("not found")
+func getCounts(messagedUsers []string, validUsers map[string]int) {
+	for _, user := range messagedUsers {
+		if _, exists := validUsers[user]; exists {
+			validUsers[user]++
+		}
 	}
-	if !elem.scheduledForDeletion {
-		return false, nil
-	}
-	delete(users, name)
-	return true, nil
 }
-
-type user struct {
-	name                 string
-	number               int
-	scheduledForDeletion bool
-}
+ 
